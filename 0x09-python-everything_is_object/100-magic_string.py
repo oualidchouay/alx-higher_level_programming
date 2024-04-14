244612b07f1183a1af26f026c798edc09a6a314a
oualidchouay <oualidchouay@gmail.com>
2024-04-14 21:19:40 +0100

    Task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-	magic_string.cout = getattr(magic_string, 'count', 0) + 1
-	return ", ".join(["BestSchool" for i in range(magic_string.count)])
-
+def magic_string(l=[]):
+    l += ["BestSchool"]
+    return ", ".join(l)
