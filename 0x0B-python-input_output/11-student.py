4fd634c8325edab83e32dde744a5810a495f4b0e
oualidchouay <oualidchouay@gmail.com>
2024-04-21 13:06:47 +0100

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,23 +1,54 @@
 #!/usr/bin/python3
-"""Student class defines a student"""
+"""Module defining the class Student based on 10-student.py"""
+
 
 class Student:
-    """defines a student"""
+    """
+    Class that defines properties of student.
+
+    Attributes:
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
+    """
     def __init__(self, first_name, last_name, age):
-        """instantiation"""
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of
-        Student instance"""
-        if attrs is not None:
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained in,
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Returns:
+            dict: dictionary representation.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for item in attrs:
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
+        return new_dict
 
     def reload_from_json(self, json):
-        """replaces all attributes of the student instance"""
-        for key, value in json.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
+        """Replaces all attributes of the Student instance.
+
+        Args:
+            json (dict): json object.
+        """
+        # print("Type json --> {}".format(type(json)))
+        self.__dict__.update(json)
