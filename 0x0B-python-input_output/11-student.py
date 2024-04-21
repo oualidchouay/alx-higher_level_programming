07783ce654342957c548f4eae5e621bdbc81a609
oualidchouay <oualidchouay@gmail.com>
2024-04-21 12:37:31 +0100

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Student class defines a student"""
 
-
 class Student:
     """defines a student"""
     def __init__(self, first_name, last_name, age):
@@ -13,11 +12,12 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of
         Student instance"""
-        if type(attrs) == list and all(type(elem) == str for elem in attrs):
+        if attrs is not None:
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
         """replaces all attributes of the student instance"""
         for key, value in json.items():
-            setattr(self, key, value)
+            if hasattr(self, key):
+                setattr(self, key, value)
