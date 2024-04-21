3d622e82ade96d914e3df071fac4bd7a44c04d0b
oualidchouay <oualidchouay@gmail.com>
2024-04-21 12:29:18 +0100

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,23 +1,47 @@
 #!/usr/bin/python3
-"""Student class defines a student"""
+"""
+    10-student: class Student
+"""
 
 
 class Student:
-    """defines a student"""
+    """
+        A class students that defines a student by:
+        Attributes:
+            first_name (str): name of student.
+            last_name (str): name of student.
+            age (int): age of student.
+        Methods:
+            __init__ - initializes the Student instance.
+            to_json - retrieves dictionary repr of Student instance.
+            reload_from_json - replaces a;; attributes of Student instance.
+    """
     def __init__(self, first_name, last_name, age):
-        """instantiation"""
+        """
+            Initialises Student instance.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """retrieves a dictionary representation of
-        Student instance"""
-        if type(attrs) == list and all(type(elem) == str for elem in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+            Args:
+                attr (list): attribute names that are to be retrieved.
+        """
+
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
 
     def reload_from_json(self, json):
-        """replaces all attributes of the student instance"""
+        """
+            replaces all attributes of the Student Instance.
+            Args:
+                json (dictionary): reload data.
+        """
         for key, value in json.items():
-            setattr(self, key, value)
+            self.__setattr__(key, value)
