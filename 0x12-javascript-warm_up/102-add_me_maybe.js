43907812105664428c0a438771931ddd7ae0283e
oualidchouay <oualidchouay@gmail.com>
2024-06-23 17:34:55 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-export const addMeMaybe = (number, theFunction) => {
-    theFunction(++number);
-};
+exports.addMeMaybe = function (number, theFunction) {
+	theFunction(++number);
+  };
