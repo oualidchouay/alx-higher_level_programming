4e741dc1fb43440cce40aa11c009e076977cbeb2
oualidchouay <oualidchouay@gmail.com>
2024-06-23 18:24:19 +0100

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+// module.exports = class Rectangle {
+//   constructor (w, h) {
+// this.width = w;
+// this.height = h;
+//   }
+// };
+
+// OR
+class Rectangle {
 	constructor (w, h) {
 	  this.width = w;
 	  this.height = h;
 	}
-  };
+  }
+
+  module.exports = Rectangle;
+  
