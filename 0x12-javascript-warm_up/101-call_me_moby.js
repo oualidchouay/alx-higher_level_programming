4145b16d7cd072b5c95d481fae0b2252f708f25a
oualidchouay <oualidchouay@gmail.com>
2024-06-23 16:49:38 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-export function callMeMoby(x, theFunction) {
-    for (let i = 0; i < x; i++) theFunction();
-};
+exports.callMeMoby = function (x, theFunction) {
+	for (let index = 0; index < x; index++) {
+	  theFunction();
+	}
+  };
+  
