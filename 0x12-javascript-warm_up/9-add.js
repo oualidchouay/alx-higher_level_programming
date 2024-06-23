cb0367d4225e7f6faa89fef52a2c5706439f6a5c
oualidchouay <oualidchouay@gmail.com>
2024-06-23 15:59:22 +0100

    0x12-Javascript-warm_up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
+const argva = process.argv[2];
+const argvb = process.argv[3];
+const a = Number(argva);
+const b = Number(argvb);
+const result = add(a, b);
 function add (a, b) {
-	return a + b;
-  }
-
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+  return (a + b);
+}
+console.log(result);
