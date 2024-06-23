bfbbd3ba838447a465024163f92184dbe0572a7f
oualidchouay <oualidchouay@gmail.com>
2024-06-23 15:57:44 +0100

    0x12-Javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(line);
-  }
-  
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < myVar.length; index++) {
+  console.log(myVar[index]);
+}
