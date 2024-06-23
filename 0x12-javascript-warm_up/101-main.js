116992a7757b0b8cf99f973c7b08b0526e8be406
oualidchouay <oualidchouay@gmail.com>
2024-06-23 17:29:20 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/101-main.js
+++ 0x12-javascript-warm_up/101-main.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
+import { callMeMoby } from '../101-call_me_moby';
+
 callMeMoby(3, function () {
   console.log('C is fun');
 });
