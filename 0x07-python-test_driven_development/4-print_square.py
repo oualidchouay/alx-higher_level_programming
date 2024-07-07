e5d4616035d5b82b7ce13e2f7138a7032eb577df
oualidchouay <oualidchouay@gmail.com>
2024-07-07 18:56:17 +0100

    0x0F. Python - Object-relational mapping

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-This is the function about  4_print_square.py
+    4-print_square Module
 """
 
 
 def print_square(size):
-    """This function prints a square with the carachter #, size >= 0"""
-    if isinstance(size, int):
+    """
+        Prints a square with the character '#'
+
+        Args:
+            size: size length of the square
+    """
+    if type(size) is not int:
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    for _ in range(size):
-        print("#" * size)
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
